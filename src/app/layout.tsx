9f135eb54aa3baeeb540b953bce1bd8a92c6c6e8
Rudy Virquin <rudy.virquin@gmail.com>
2026-06-19 09:35:59 +0200

    fix: hydration mismatch extension navigateur - ajout suppressHydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
       lang="fr"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
